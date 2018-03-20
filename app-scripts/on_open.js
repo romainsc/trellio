1532ad954c69dbe44c3001f10bcde7c0c7a41f78
Romain Chantereau <romain@chantereau.fr>
2018-03-20 18:39:09 +0100

    Change menu title (and experience cslap push->spreadsheet update)

--- app-scripts/on_open.js
+++ app-scripts/on_open.js
@@ -1,5 +1,5 @@
 function onOpen(){
   var ss = SpreadsheetApp.getActiveSpreadsheet();
-  var menu_entries = [{name: "Display Available Boards", functionName: "display_boards"},{name: "Upload cards from “cards” sheet", functionName: "upload"}];
+  var menu_entries = [{name: "Display Available Boards", functionName: "display_boards"},{name: "Synchronize cards from “cards” sheet", functionName: "upload"}];
   ss.addMenu("Trello", menu_entries);
  }
